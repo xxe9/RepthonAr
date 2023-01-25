86b215cf3753d71f08608c875c348c3fcfe6060e
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-25 21:26:31 +0300

    Update حماية الخاص.py

--- jepthon/plugins/حماية الخاص.py
+++ jepthon/plugins/حماية الخاص.py
@@ -901,6 +901,6 @@ async def approve_p_m(event):
     await edit_or_reply(
         event,
         APPROVED_PMs,
-        file_name="قائـمة الحـماية جـيبثون.txt",
-        caption="قائـمة الـمسموح لـهم الـحالية\n سـورس جـيبثون الـعربي \n @JepThon",
+        file_name="قائـمة الحـماية ريبـــثون.txt",
+        caption="قائـمة الـمسموح لـهم الـحالية\n سـورس ريبـــثون الـعربي \n @Repthon",
     )#ترجمه وكتابة فريق جـيبثون
