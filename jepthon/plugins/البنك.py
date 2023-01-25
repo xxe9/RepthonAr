22da2cc2609f5d6904b3a293db6f7f88cbe02b8d
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-25 20:52:43 +0300

    Update البنك.py

--- jepthon/plugins/البنك.py
+++ jepthon/plugins/البنك.py
@@ -29,8 +29,8 @@ async def amireallyalive(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     EMOJI = gvarstatus("ALIVE_EMOJI") or "✇ ◅"
-    PING_TEXT = gvarstatus("PING_TEXT") or "**[ 𝗜 𝗝𝘂𝘀𝘁 𝗔𝘀𝗸𝗲𝗱 𝗙𝗼𝗿 𝗦𝗼𝗺𝗲 𝗣𝗲𝗮𝗰𝗲 🎀 ](t.me/Jepthon)**"
-    PING_IMG = gvarstatus("PING_PIC") or Config.P_PIC or "https://telegra.ph/file/fb62d70ce09f4a78dfc86.jpg"
+    PING_TEXT = gvarstatus("PING_TEXT") or "**[ 𝗜 𝗝𝘂𝘀𝘁 𝗔𝘀𝗸𝗲𝗱 𝗙𝗼𝗿 𝗦𝗼𝗺𝗲 𝗣𝗲𝗮𝗰𝗲 🎀 ](t.me/Repthon)**"
+    PING_IMG = gvarstatus("PING_PIC") or Config.P_PIC or "https://graph.org/file/ff78ec5d463082b7b0eb8.jpg"
     jepthon_caption = gvarstatus("PING_TEMPLATE") or temp
     caption = jepthon_caption.format(
         PING_TEXT=PING_TEXT,
