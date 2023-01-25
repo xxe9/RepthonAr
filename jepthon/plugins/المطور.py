7ba760a862fcc3333801e8c4f4ea7cad5596579d
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-25 21:30:17 +0300

    Update المطور.py

--- jepthon/plugins/المطور.py
+++ jepthon/plugins/المطور.py
@@ -44,7 +44,7 @@ async def amireallyalive(event):
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين تيبثون\n"
+        cat_caption = f"مطورين ريبـــثون\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         cat_caption += f"- المطور  : @E_7_V\n"
         cat_caption += f"- المطور  : @PPF22\n"
